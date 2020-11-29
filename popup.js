2e0ae1360f7829fd13aa0f642a4f47f5aaae0c9d
Lenny Wu <elenewu88@gmail.com>
2020-11-29 13:26:30 -0800

    cleaned up code and changed appearance

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 'use strict';
-//const { Session } = require("inspector");
+
 var url = "www.google.com";
+
+function parse(data) {
+    var parser = new DOMParser();
+    var html = parser.parseFromString(data, "text/html");
+    return html.getElementById("output").lastChild.innerHTML;
+}
 
 function handleClick() {
 
@@ -11,20 +17,41 @@ function handleClick() {
         }
     );
 
-    const urlsrc = document.getElementById("urlsrc");
+    let urlsrc = document.getElementById("urlsrc");
     sessionStorage.setItem("urlsrc", urlsrc.value);
 
     if (urlsrc && urlsrc.value === "ShadyURL") {
-        console.log(url);
+        if (sessionStorage.getItem("shadyurl_clicked"))
+            return false;
+
+        fetch('https://cors-anywhere.herokuapp.com/http://www.shadyURL.com/create.php?', {
+            method: 'POST',
+            headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+            "Referer": "https://www.google.com/",
+            "X-Requested-With": "XMLHttpRequest" },
+            body: "myUrl={url}"
+        })
+        .then(response => response.text())
+        .then((resdata) => {
+            console.log(resdata);
+            resdata = parse(resdata);
+            sessionStorage.setItem("url", resdata);
+            document.getElementById("link").innerHTML = resdata;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+        sessionStorage.setItem("verylegit_clicked", true);
     }
+
     else {
         if (sessionStorage.getItem("verylegit_clicked"))
             return false;
 
         fetch('https://verylegit.link/sketchify', {
             method: 'POST',
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded' },
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: "long_url={url}"
         })
         .then(response => response.text())
@@ -35,6 +62,7 @@ function handleClick() {
         .catch((err) => {
             console.log(err);
         });
+        
         sessionStorage.setItem("verylegit_clicked", true);
     }
     return false;
