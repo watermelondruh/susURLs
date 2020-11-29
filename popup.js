5cc5990964bfb351037a8b05b374610116a1154f
Lenny Wu <elenewu88@gmail.com>
2020-11-29 00:51:06 -0800

    sadge

--- popup.js
+++ popup.js
@@ -2,38 +2,36 @@
 
 //const { Session } = require("inspector");
 
-let button = document.getElementById("button");
-let urlsrc = document.getElementById("urlsrc");
-let link = document.getElementById("link");
-let url = "www.google.com";
-
 function handleClick() {
-    sessionStorage.setItem("urlsrc", urlsrc.value);
+    //sessionStorage.setItem("urlsrc", urlsrc.value);
+    
+    const data = {'long_url' : 'twitter.com'};
+    const response = fetch('https://verylegit.link/sketchify', {
+        method: 'POST',
+        headers: { 
+            'Content-Length': 20,
+            'Content-Type': 'application/x-www-form-urlencoded' },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then((resdata) => {
+        alert(resdata, "bruh");
+    })
+    .catch((err) => {
+        //sessionStorage.setItem("url", err);
+        console.log(err);
+    });
+}
+
+window.onload = function () {
+    document.getElementById("button").onclick = handleClick;
+    // display current link
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
         function(tabs) {
             url = tabs[0].url;
-            sessionStorage.setItem("url", url);
+            //sessionStorage.setItem("url", url);
+            document.getElementById("link").innerHTML = url;
         }
     );
-    
-    if (urlsrc != null && urlsrc.value === "ShadyURL") {
-        console.log(url);
-    }
-    else {
-        console.log(url);
-    }
-
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    button = document.getElementById("button");
-    button.onclick = function f() {
-        handleClick();
-    }
-    urlsrc = document.getElementById("urlsrc");
-    urlsrc.value = sessionStorage.getItem("urlsrc");
-    link = document.getElementById("link");
-    link.innerHTML = sessionStorage.getItem("url");
-});
+};
 
