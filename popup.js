b287ce3fca49523dd849afbb07717e4c1edabe91
Lenny Wu <elenewu88@gmail.com>
2020-11-29 14:53:12 -0800

    fixed fetch header syntax

--- popup.js
+++ popup.js
@@ -2,12 +2,6 @@
 
 var url = "www.google.com";
 
-function parse(data) {
-    var parser = new DOMParser();
-    var html = parser.parseFromString(data, "text/html");
-    return html.getElementById("output").lastChild.innerHTML;
-}
-
 function handleClick() {
 
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
@@ -17,60 +11,32 @@ function handleClick() {
         }
     );
 
-    let urlsrc = document.getElementById("urlsrc");
-    sessionStorage.setItem("urlsrc", urlsrc.value);
+    if (sessionStorage.getItem("verylegit_clicked"))
+        return false;
+    
+    console.log(url);
 
-    if (urlsrc && urlsrc.value === "ShadyURL") {
-        if (sessionStorage.getItem("shadyurl_clicked"))
-            return false;
+    fetch('https://verylegit.link/sketchify', {
+        method: 'POST',
+        headers: { 'Content-Length': 20, 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' },
+        body: "long_url="+url
+    })
+    .then(response => response.text())
+    .then((resdata) => {
+        sessionStorage.setItem("url", resdata);
+        document.getElementById("link").innerHTML = resdata;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+    sessionStorage.setItem("verylegit_clicked", true);
 
-        fetch('https://cors-anywhere.herokuapp.com/http://www.shadyURL.com/create.php?', {
-            method: 'POST',
-            headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-            "Referer": "https://www.google.com/",
-            "X-Requested-With": "XMLHttpRequest" },
-            body: "myUrl={url}"
-        })
-        .then(response => response.text())
-        .then((resdata) => {
-            console.log(resdata);
-            resdata = parse(resdata);
-            sessionStorage.setItem("url", resdata);
-            document.getElementById("link").innerHTML = resdata;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-        sessionStorage.setItem("verylegit_clicked", true);
-    }
-
-    else {
-        if (sessionStorage.getItem("verylegit_clicked"))
-            return false;
-
-        fetch('https://verylegit.link/sketchify', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: "long_url={url}"
-        })
-        .then(response => response.text())
-        .then((resdata) => {
-            sessionStorage.setItem("url", resdata);
-            document.getElementById("link").innerHTML = resdata;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-        
-        sessionStorage.setItem("verylegit_clicked", true);
-    }
     return false;
 }
 
 window.onload = function () {
     document.getElementById("button").onclick = handleClick;
-    // display current link
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
         function(tabs) {
            url = tabs[0].url;
